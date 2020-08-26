e24f7d806ed399dac14007e312a597146fc11b06
djiang2 <danieljng2005@gmail.com>
2020-08-26 03:31:26 -0400

    edit -help text

--- WaveformPlayer.cc
+++ WaveformPlayer.cc
@@ -14,12 +14,12 @@ int main(int argc, char* argv[]) {
 		std::cout << "Try './WaveformPlayer -help' for details" << std::endl;
 		return 1;
 	} else if (strcmp(argv[1], "-help") == 0) {
-		std::cout << "This program plays a .wav file and displays some visuals. Press 'q' to stop playing. This program calculates an entire spectrogram for the file, which can take a lot of RAM (a 4 minute 2-channel file at 44100 Hz with window = 4096 and overlap = 2048 took about 2.2 GB. Disable the spectrum by using the option 'nospec' to use less RAM." << std::endl;
+		std::cout << "This program plays a .wav file and displays some visuals. Press 'q' to stop playing. This program calculates an entire spectrogram for the file, which can take a lot of RAM (a 4 minute 2-channel file at 44100 Hz with window = 4096 and overlap = 2048 took about 2.2 GB. Disable the spectrum by using the option '-nospec' to use less RAM." << std::endl;
 		std::cout << "Options:" << std::endl;
 		std::cout << "  -debug : Generates logs for data of the WAV file. 'logNormData.txt' contains the data chunk normalized, 'logHeader.txt' contains header information, 'logRawBytes.txt' displays the data chunk in hex, and 'logNumData.txt' displays the data chunk numerically." << std::endl;
 		std::cout << "  -nospec : Does not generate spectrogram or display spectrum while playing." << std::endl;
 		std::cout << "  -custom_spec : will prompt the user to enter an integer for the window and overlap (both must be positive integers, with window > overlap). This means each frame is window - overlap apart. Will also ask if a Hann window will be used." << std::endl;
-		std::cout << "  By default (no options), uses window = 4096, overlap = 2048, with Hann windowing." << std::endl;
+		std::cout << "  By default (no options), uses window = 2048, overlap = 1024, with Hann windowing." << std::endl;
 		return 0;
 	}
 
